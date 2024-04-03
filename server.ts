d443e6394b70b87a560243d98553db3e2be2a0fc
GreeneGunnar14 <bryce.greene@students.fhu.edu>
2024-04-03 12:31:00 -0500

    add logging to server

--- server.ts
+++ server.ts
@@ -1,12 +1,27 @@
 // server.ts - Imports the app and sets it to listen on the indicated port.
 
+// Import the app from src
 import app from "./src/app";
 
+// Import morgan library for logging http requests
+import morgan from "morgan";
+
+// Read port from environment variables
 const PORT = process.env.PORT;
 
+// Read debugging mode from environment variables
+const DEBUG = process.env.DEBUG === "true" || false;
+
+// Set logging level based on debug mode and add morgan middleware to app
+const logging_level = DEBUG ? "dev" : "tiny";
+app.use(morgan(logging_level));
+
+// Have the app listen on the desired port
 const server = app.listen(PORT, () => {
   const HOST = process.env.HOSTNAME || "localhost";
   console.log(`Listening on ${HOST}:${PORT}`);
+  DEBUG ??
+    console.log("Running in debug mode. Turn of debugging for production.");
 });
 
 // Export server for testing
