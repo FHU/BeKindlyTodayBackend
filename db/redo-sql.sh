a7944709fcf3dc48ef4c4439d5c81a1ad6fd2aa3
Evan Vance <72946268+evandvance@users.noreply.github.com>
2024-04-04 13:00:59 -0500

    Changed sleep to 10 seconds to account for first time downloading Postgres image

--- db/redo-sql.sh
+++ db/redo-sql.sh
@@ -8,8 +8,8 @@ MIGRATION_NUMBER=`find . -mindepth 1 -maxdepth 1 -type d | wc -l`
 
 #This sleep is to make sure the postgres db is up all the way before running commands against it. I looked at ways to check to make sure
 #it was running using things like healthchecks, curl, nc, etc but I find this solution to be the simplest since it will never take more
-#than 5ish seconds to run a brand new postgres instance
-sleep 5
+#than 10ish seconds to run a brand new postgres instance
+sleep 10
 
 npx prisma migrate dev --schema ../prisma/schema.prisma --name MIG_$MIGRATION_NUMBER
 
