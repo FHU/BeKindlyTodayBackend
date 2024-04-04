e85e00b7d57595347d5cb33a146853b1f40442a3
Bryce <greene8616@gmail.com>
2024-04-04 18:41:04 -0500

    write the memories route for users

--- src/api/users.ts
+++ src/api/users.ts
@@ -11,9 +11,25 @@ const prisma = new PrismaClient();
 
 // Routes go here (get, post, put, delete)
 
-users.get("/", async (req, res) => {
-  const users = await prisma.users.findMany();
-  res.json(users);
+users.use("/memories", async (req, res) => {
+  try {
+    // Find all user_completions for the current user. Include the completion model for each user_completion
+    const user_completions = await prisma.user_completions.findMany({
+      where: {
+        user_id: req.body.user.id,
+      },
+      include: { completion: true },
+    });
+
+    const completions = user_completions.map((user_completion) => {
+      return user_completion.completion;
+    });
+
+    res.json(completions);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error." });
+  }
 });
 
 // Export the router for use in the app.
