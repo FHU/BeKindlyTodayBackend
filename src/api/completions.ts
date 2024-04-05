38d515bafe16d5cc4383aef4ccbdddc8b7c68091
Bryce <greene8616@gmail.com>
2024-04-05 10:51:55 -0500

    begin working on post route for completions

--- src/api/completions.ts
+++ src/api/completions.ts
@@ -10,4 +10,19 @@ const completions = express.Router();
 // Create prisma client
 const prisma = new PrismaClient();
 
+completions.post("/", async (req, res) => {
+  try {
+    const challenge = await prisma.challenges.findUnique({
+      where: {
+        date: new Date().toISOString().slice(0, 10),
+      },
+    });
+
+    // FIXME Schema does not easily support enforcing uniqueness completions based on user + challenge
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 export default completions;
