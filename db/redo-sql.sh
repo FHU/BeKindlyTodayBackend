2f3ab791092a5ed0e7a2c7db7f83151834cc33b8
Evan <evandvance@gmail.com>
2024-04-07 14:18:23 -0500

    Changed script to work with what others are doing

--- db/redo-sql.sh
+++ db/redo-sql.sh
@@ -4,24 +4,22 @@
 
 docker run -dp 5432:5432 -e POSTGRES_PASSWORD=Mydatabasepassword1 --name bkt_db_c postgres:16-alpine 
 
-MIGRATION_NUMBER=`find . -mindepth 1 -maxdepth 1 -type d | wc -l`
-
 #This sleep is to make sure the postgres db is up all the way before running commands against it. I looked at ways to check to make sure
 #it was running using things like healthchecks, curl, nc, etc but I find this solution to be the simplest since it will never take more
 #than 10ish seconds to run a brand new postgres instance
 sleep 10
 
-npx prisma migrate dev --schema ../prisma/schema.prisma --name MIG_$MIGRATION_NUMBER
+npx prisma migrate deploy
 
 echo "Dumping Schema"
-pg_dump --dbname=postgresql://postgres:Mydatabasepassword1@localhost:5432/postgres > ./prod/schema.sql
+pg_dump --dbname=postgresql://postgres:Mydatabasepassword1@localhost:5432/postgres > ./db/prod/schema.sql
 
 #New dummy data might be needed when a schema change is made
 echo "Inserting dummy data"
-psql --dbname=postgresql://postgres:Mydatabasepassword1@localhost:5432/postgres -f ./dummydata.sql
+psql --dbname=postgresql://postgres:Mydatabasepassword1@localhost:5432/postgres -f ./db/dummydata.sql
 
 echo "Backing up database"
-pg_dump --dbname=postgresql://postgres:Mydatabasepassword1@localhost:5432/postgres > ./dev/dev_data.sql
+pg_dump --dbname=postgresql://postgres:Mydatabasepassword1@localhost:5432/postgres > ./db/dev/dev_data.sql
 
 docker stop bkt_db_c
 docker rm bkt_db_c
