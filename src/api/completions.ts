003d8ff88b9123137327d4023d8b9d360562ff22
Bryce <greene8616@gmail.com>
2024-04-08 01:01:16 -0500

    write get methods for completions and completion count

--- src/api/completions.ts
+++ src/api/completions.ts
@@ -3,12 +3,108 @@
 // Import dependencies
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import { Completion } from "@prisma/client";
 
 // Create router
 const completions = express.Router();
 
 // Create prisma client
 const prisma = new PrismaClient();
+
+async function filter_completions(
+  filter: string | undefined,
+  user_id: number
+): Promise<Completion[]> {
+  let completions: Completion[];
+
+  // No filter is same as filter for 'all'
+  if (filter === undefined || filter === "all") {
+    completions = await prisma.completion.findMany(); // Find all completions
+
+    // Filter completions for today's challenge
+  } else if (filter === "today") {
+    // Get today's challenge
+    const challenge = await prisma.challenge.findUnique({
+      where: { date: new Date().toISOString().slice(0, 10) },
+    });
+
+    // Return empty list if no challenge for today.
+    if (challenge === null) {
+      return [];
+    }
+
+    // Find completions based on challenge
+    completions = await prisma.completion.findMany({ where: { challenge } });
+
+    // Filter completions for logged in user
+  } else if (filter === "user") {
+    // Find completions based on user
+    completions = await prisma.completion.findMany({ where: { user_id } });
+  } else {
+    completions = []; // Don't know why ts doesn't recognize that completions is defined without this else statement.
+  }
+
+  return completions;
+}
+
+// Get all completions that pass filter
+completions.get("/", async (req, res) => {
+  try {
+    // Get filter from request query
+    const filter = req.query.filter;
+
+    // Get the user id from the request
+    const user_id = parseInt(req.body.user.id);
+
+    // Ensure filter matches allowed values
+    if (!["all", "today", "user", undefined].includes(filter?.toString())) {
+      res.status(400).json({
+        message:
+          "Bad Request. Filter, if included, must be 'all', 'user', or 'today'",
+      });
+      return;
+    }
+
+    // Get the completions based on the filter
+    const completions = await filter_completions(filter?.toString(), user_id);
+
+    // Return the completions
+    res.json(completions);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Get the count of completions that pass a filter
+completions.get("/count", async (req, res) => {
+  try {
+    // Get filter from request query
+    const filter = req.query.filter;
+
+    // Get user id from request body
+    const user_id = parseInt(req.body.user.id);
+
+    // Check that filter matches allowed filters
+    if (!["all", "today", "user", undefined].includes(filter?.toString())) {
+      res.status(400).json({
+        message:
+          "Bad Request. Filter, if included, must be 'all', 'user', or 'today'",
+      });
+      return;
+    }
+
+    // Get completions and count the number
+    const completions = await filter_completions(filter?.toString(), user_id);
+    const count = completions.length;
+
+    // Return the count
+    res.json({ count });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 // Get completion based on id
 completions.get("/:id", async (req, res) => {
