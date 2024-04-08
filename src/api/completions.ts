4e94e8430565dcbf361c1dd35e93a05c70f261b8
Bryce <greene8616@gmail.com>
2024-04-08 00:24:27 -0500

    add comments for documentation and complete post method

--- src/api/completions.ts
+++ src/api/completions.ts
@@ -10,9 +10,13 @@ const completions = express.Router();
 // Create prisma client
 const prisma = new PrismaClient();
 
+// Get completion based on id
 completions.get("/:id", async (req, res) => {
   try {
+    // declare variable for completion's id
     let completion_id;
+
+    // Parse the id parameter provided to filter out bad request
     try {
       completion_id = parseInt(req.params.id);
     } catch (err) {
@@ -22,15 +26,18 @@ completions.get("/:id", async (req, res) => {
       return;
     }
 
+    // Get the completion from the database
     const completion = await prisma.completion.findUnique({
       where: { id: completion_id },
     });
 
+    // Check for 404 errors
     if (completion === null) {
       res.status(404).json({ message: "Not Found" });
       return;
     }
 
+    // Send the json object of the completion
     res.json(completion);
   } catch (err) {
     console.error(err);
@@ -38,28 +45,35 @@ completions.get("/:id", async (req, res) => {
   }
 });
 
+// Post method for completions
 completions.post("/", async (req, res) => {
   try {
+    // Get the daily challenge
     const challenge = await prisma.challenge.findUnique({
       where: {
         date: new Date().toISOString().slice(0, 10),
       },
     });
 
+    // Check that the challenge was found
     if (challenge === null) {
       res.status(404).json({ message: "No challenge found for current date." });
       return;
     }
 
+    // Get the user id from the request
     const { user_id } = req.body.user; // FIXME Figure out how Kinde will integrate then change this to find the user or user id
-    const challenge_id = challenge.id;
 
+    // Create object to query completions based on user id an challenge id
+    const challenge_id = challenge.id;
     const user_id_challenge_id = { user_id, challenge_id };
 
+    // Check for existing completions
     const completion = await prisma.completion.findUnique({
       where: { user_id_challenge_id },
     });
 
+    // Redirect to existing completion if it already exists
     if (completion !== null) {
       res
         .setHeader("location", `api/v1/completions/${completion.id}`)
@@ -67,7 +81,20 @@ completions.post("/", async (req, res) => {
       return;
     }
 
-    res.json(completion);
+    // Get the description from the request
+    const { description } = req.body;
+
+    // Create the new completion
+    const new_completion = await prisma.completion.create({
+      data: {
+        challenge_id,
+        user_id,
+        description,
+        date: new Date(), // Set date to current date & time
+      },
+    });
+
+    res.json(new_completion);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal Server Error" });
