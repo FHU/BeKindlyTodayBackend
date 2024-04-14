b13199276f7765af85b515a7cb1c5f24672881f9
Evan <evandvance@gmail.com>
2024-04-13 19:58:57 -0500

    Changed startup readme for dev

--- db/get_challenges_from_spreadsheet.py
+++ db/get_challenges_from_spreadsheet.py
@@ -4,4 +4,11 @@ sheet_id = "1YQ5sRGze8MS6edGRFEHMjyDEnrYjvst5Hdy9HB5eQ_o"
 
 df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export=?format=csv")
 
-print(df)
+def make_values(prompt, description, twist, date, source, image):
+    return f"\n('{prompt}', '{description}', '{date}', '{source}', '{image}'),"
+
+test =""
+
+sql_template = f'INSERT INTO "challenge" (prompt, suggestion, date, source, image) VALUES{test}'.rstrip(",") + ";"
+
+with open("./challenges.sql", "w") as file:
