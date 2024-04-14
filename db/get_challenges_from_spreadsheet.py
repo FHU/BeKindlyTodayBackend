b3996c3160855609df8f3a9d9c9f5bb00216d12c
Evan <evandvance@gmail.com>
2024-04-13 20:28:49 -0500

    Added script to make sql for seeding challenges

--- db/get_challenges_from_spreadsheet.py
+++ db/get_challenges_from_spreadsheet.py
@@ -2,13 +2,17 @@ import pandas as pd
 
 sheet_id = "1YQ5sRGze8MS6edGRFEHMjyDEnrYjvst5Hdy9HB5eQ_o"
 
-df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export=?format=csv")
+df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
 
-def make_values(prompt, description, twist, date, source, image):
-    return f"\n('{prompt}', '{description}', '{date}', '{source}', '{image}'),"
+def make_values(prompt, description, twist, date, source, rating, image):
+    return f"\n  ('{prompt}', '{description}', '{twist}', '{date}', '{source}', '{rating}', '{image}'),"
 
-test =""
+values = ""
 
-sql_template = f'INSERT INTO "challenge" (prompt, suggestion, date, source, image) VALUES{test}'.rstrip(",") + ";"
+for row in df.itertuples():
+    values += make_values(row[1], row[2], row[3], row[4], row[5], row[6], row[7])
 
-with open("./challenges.sql", "w") as file:
+sql_template = f'INSERT INTO "challenge" (prompt, suggestion, date, source, image) VALUES{values}'.rstrip(",") + ";"
+
+with open("./db/challenges.sql", "w") as file:
+    file.write(sql_template)
