76f039834154d195df5cb8d5887524ce53557d8f
Evan <evandvance@gmail.com>
2024-04-14 07:53:15 -0500

    Made Script more readable

--- db/get_challenges_from_spreadsheet.py
+++ db/get_challenges_from_spreadsheet.py
@@ -9,15 +9,24 @@ print("Getting data from spreadsheet...")
 sheet_id = "1YQ5sRGze8MS6edGRFEHMjyDEnrYjvst5Hdy9HB5eQ_o"
 df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
 
-values = ""
+sql_template = f'INSERT INTO "challenge" (prompt, suggestion, twist, date, source, rating, image) VALUES'
 
 print("Forming Sql...")
 #I do not know pandas well, so this is what I did. Also I know this is bad but itll work for now
 for row in df.itertuples():
-    #Each index of the row coresponds to the column that it is in in the spreadsheet. I am using the index for the day from the date this script is run.
-    values += make_values(str(row[1]).replace("'",""), str(row[2]).replace("'",""), str(row[3]).replace("'",""), row[0], str(row[5]).replace("'",""), str(row[6]).replace("'",""), str(row[7]).replace("'",""))
+    #Each index of the row coresponds to the column that it is in in the spreadsheet
+    
+    prompt = str(row[1]).replace("'","")
+    description = str(row[2]).replace("'","")
+    twist = str(row[3]).replace("'","")
+    date = f"NOW() + interval '{row[0]} days'" #The index is the ammount of days past now that the date is being made
+    source = str(row[5]).replace("'","")
+    rating = 0
+    image = str(row[7]).replace("'","")
 
-sql_template = f'INSERT INTO "challenge" (prompt, suggestion, twist, date, source, rating, image) VALUES{values}'.rstrip(",") + ";"
+    sql_template += f"\n  ('{prompt}', '{description}', '{twist}', {date}, '{source}', {rating}, '{image}'),"
+
+sql_template = sql_template.rstrip(",") + ";"
 
 print("Writing SQL to file...")
 with open("./db/challenges.sql", "w") as file:
