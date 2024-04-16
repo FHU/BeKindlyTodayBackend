c7b527cbab3f11ae835154a5ec6389ec412fcced
Evan <evandvance@gmail.com>
2024-04-16 09:29:41 -0500

    added missing /

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 // app.ts - Creates the express app and defines routes.
 
 // Import dependencies
-import express from "express";
-import morgan from "morgan";
+import express from 'express';
+import morgan from 'morgan';
 
 // Import the routers for the app
-import users from "./api/users";
-import challenges from "./api/challenges";
-import completions from "./api/completions";
+import users from './api/users';
+import challenges from './api/challenges';
+import completions from './api/completions';
 
 // Create the app
 const app = express();
@@ -16,18 +16,18 @@ const app = express();
 app.use(express.json());
 
 // Set debug based on environment variables
-const DEBUG = process.env.DEBUG?.toLowerCase() === "true" || false;
+const DEBUG = process.env.DEBUG?.toLowerCase() === 'true' || false;
 
 // Set logging level based on debug mode and add morgan middleware to app
-const logging_level = DEBUG ? "dev" : "tiny";
+const logging_level = DEBUG ? 'dev' : 'tiny';
 app.use(morgan(logging_level));
 
 // Use the users router in the corresponding route.
-app.use("/api/v1/users", users);
+app.use('/api/v1/users', users);
 
-app.use("/api/v1/challenges", challenges);
+app.use('/api/v1/challenges', challenges);
 
-app.use("api/v1/completions", completions);
+app.use('/api/v1/completions', completions);
 
 // Export the app for the server to run.
 export default app;
