23ac2e0c6cd2651c3557f8ce2e0eba906bf6dccc
Evan <evandvance@gmail.com>
2024-04-15 22:34:28 -0500

    Editted scripts to build/use upsert

--- db/get_challenges_from_spreadsheet.py
+++ db/get_challenges_from_spreadsheet.py
@@ -4,7 +4,7 @@ print("Getting data from spreadsheet...")
 sheet_id = "1YQ5sRGze8MS6edGRFEHMjyDEnrYjvst5Hdy9HB5eQ_o"
 df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
 
-sql_template = f'INSERT INTO "challenges" (prompt, suggestion, twist, date, source, rating, image) VALUES'
+sql_template = f'INSERT INTO "challenges" (prompt, spanish_prompt, suggestion, twist, spanish_twist, date, source, rating, image) VALUES'
 
 print("Forming Sql...")
 #I do not know pandas well, so this is what I did. Also I know this is bad but itll work for now
@@ -14,22 +14,26 @@ for row in df.itertuples():
     #The replaces here is to remove ' which breaks the way the sql is written. In future iterations, the sql should be changed to not need this
     #but for now its fine.
     prompt = str(row[1]).replace("'","")
-    description = str(row[2]).replace("'","")
-    twist = str(row[3]).replace("'","")
+    spanish_prompt = str(row[2]).replace("'","")
+    description = str(row[3]).replace("'","")
+    twist = str(row[4]).replace("'","")
+    spanish_twist = str(row[5]).replace("'","")
     date = f"NOW() + interval '{row[0]} days'" #The index is the ammount of days past now that the date is being made
-    source = str(row[5]).replace("'","")
+    source = str(row[6]).replace("'","")
     rating = 0
-    image = str(row[7]).replace("'","")
+    image = str(row[8]).replace("'","")
 
-    sql_template += f"\n  ('{prompt}', '{description}', '{twist}', {date}, '{source}', '{rating}', '{image}'),"
+    sql_template += f"\n  ('{prompt}', '{spanish_prompt}', '{description}', '{twist}', '{spanish_twist}', {date}, '{source}', '{rating}', '{image}'),"
 
 sql_template = sql_template.rstrip(",") + '''
 
 ON Conflict(prompt)
 DO UPDATE SET
-    prompt = EXCLUDED.prompt
-    suggestion = EXCLUDED.suggestion
-    twist = EXCLUDED.twist
+    prompt = EXCLUDED.prompt,
+    spanish_prompt = EXCLUDED.spanish_prompt,
+    suggestion = EXCLUDED.suggestion,
+    twist = EXCLUDED.twist,
+    spanish_twist = EXCLUDED.spanish_twist,
     image = EXCLUDED.image;
 '''
 
--- db/redo-sql.sh
+++ db/redo-sql.sh
@@ -20,7 +20,7 @@ sleep 5
 npx prisma migrate deploy
 
 echo "Inserting Challenges"
-psql --dbname=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${POSTGRES_USER} -f ./db/challenges.sql
+psql --dbname=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${POSTGRES_USER} -f ./db/challenges_upsert.sql
 
 echo "Dumping Production Seed"
 pg_dump --dbname=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${POSTGRES_USER} > ./db/prod/seed.sql
@@ -42,7 +42,7 @@ sleep 5
 npx prisma migrate deploy
 
 echo "Inserting Challenges"
-psql --dbname=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${POSTGRES_USER} -f ./db/challenges.sql
+psql --dbname=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${POSTGRES_USER} -f ./db/challenges_upsert.sql
 
 #New dummy data might be needed when a schema change is made
 echo "Inserting dummy data"
