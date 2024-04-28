61327fad2cc4f747afe75dc72c16e99dd28b4545
Evan <evandvance@gmail.com>
2024-04-28 08:16:39 -0500

    Add more dummy data

--- db/sql/dummydata.sql
+++ db/sql/dummydata.sql
@@ -15,6 +15,13 @@ INSERT INTO "users" (username, kinde_id) VALUES
 -- Dummy Completions
 INSERT INTO "completions" (description, date, user_id, challenge_id) VALUES
   ('Completion 1 description', NOW() + interval '0 day', 1, 1),
+  ('Completion 1 description', NOW() + interval '1 day', 1, 2),
+  ('Completion 1 description', NOW() + interval '2 day', 1, 3),
+  ('Completion 1 description', NOW() + interval '3 day', 1, 4),
+  ('Completion 1 description', NOW() + interval '4 day', 1, 5),
+  ('Completion 1 description', NOW() + interval '5 day', 1, 6),
+  ('Completion 1 description', NOW() + interval '6 day', 1, 7),
+  ('Completion 1 description', NOW() + interval '7 day', 1, 8),
   ('Completion 2 description', NOW() + interval '1 day', 2, 2),
   ('Completion 3 description', NOW() + interval '2 day', 3, 3),
   ('Completion 4 description', NOW() + interval '3 day', 4, 4),
