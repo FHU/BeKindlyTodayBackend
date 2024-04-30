a163867a08fa74db14e1b1ee0aee91ce1cb2a12c
Evan <evandvance@gmail.com>
2024-04-30 09:07:18 -0500

    Ended the fever dream

--- src/compute_streaks.ts
+++ src/compute_streaks.ts
@@ -3,12 +3,12 @@ import { Completion } from '@prisma/client';
 export function compute_streak(completions: Completion[]) {
   if (completions.length === 0) return 0;
 
-  let streak = 1;
-
   const completionDates = completions.map(
     (completion) =>
       new Date(new Date(completion.date).setHours(0, 0, 0, 0)).getTime() //This is done to not edit original completions array
   );
+
+  completionDates.sort();
 
   const DAY_IN_MS = 86400000;
   const TODAY = new Date(new Date().setHours(0, 0, 0, 0)).getTime();
@@ -22,20 +22,19 @@ export function compute_streak(completions: Completion[]) {
    * the streak number would be returned.
    */
   let nextStreakDate = new Date(completionDates[curr] - DAY_IN_MS).getTime();
-  let nextCompletionDate = completionDates[curr - 1];
 
   if (completionDates[curr] !== TODAY && completionDates[curr] !== YESTERDAY)
     return 0;
 
+  let streak = 1;
   //While the next day in the array of completions == next day if there was a streak
-  while (nextCompletionDate == nextStreakDate) {
+  while (completionDates[curr - 1] == nextStreakDate) {
     streak++;
     curr--;
 
     nextStreakDate = new Date(completionDates[curr] - DAY_IN_MS).getTime();
-    nextCompletionDate = completionDates[curr - 1];
 
-    if (nextCompletionDate === undefined) break;
+    if (completionDates[curr - 1] === undefined) break;
   }
 
   return streak;
