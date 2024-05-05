97ed6353b625ee819b2f05e9572013bcb9177dc0
Sam Flowers <111446369+sflowers51@users.noreply.github.com>
2024-05-04 20:13:25 -0500

    made the different suites using suite() and hopefullt cleaned up some things with posting and deleting though not sure

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,17 +1,17 @@
-import { describe } from "node:test";
-import { test, expect, request } from "vitest";
+import { suite } from "node:test";
+import { test, expect, request, suite } from "vitest";
 import request from "supertest";
 
 const PORT = process.env.PORT || 5000;
-const URL = `http://0.0.0.0:${PORT}`;
+const URL = `http://127.0.0.1:${PORT}`;
 
 test("should return status code 200, 'pointless test'", async () => {
   const res = await request(URL).get("/api/");
   expect(res.statusCode).toBe(200);
 });
 
-describe("USERS SUITE", () => {
-  describe("/:id SUITE", () => {
+suite("USERS SUITE", () => {
+  suite("/:id SUITE", () => {
     test("should get user with id 1 and return status code 200", async () => {
       const res = await request(URL).get("/api/v1/users/1");
       expect(res.statusCode).toBe(200);
@@ -28,7 +28,7 @@ describe("USERS SUITE", () => {
     });
   });
 
-  describe("/stats SUITE", () => {
+  suite("/stats SUITE", () => {
     test("should get stats for user with id 1 and return status code 200", async () => {
       const res = await request(URL).get("/api/v1/users/stats").send({ id: 3 });
       expect(res.statusCode).toBe(200);
@@ -42,7 +42,7 @@ describe("USERS SUITE", () => {
     });
   });
 
-  describe("/bio SUITE", () => {
+  suite("/bio SUITE", () => {
     test("should update bio of user with id 1 and return status code 200", async () => {
       const res = await request(URL)
         .put("/api/v1/users/bio")
@@ -58,7 +58,7 @@ describe("USERS SUITE", () => {
     });
   });
 
-  describe("/profilepicture SUITE", () => {
+  suite("/profilepicture SUITE", () => {
     test("should update profilepicture of user with id 1 and return status code 200", async () => {
       const res = await request(URL)
         .put("/api/v1/users/profilepicture")
@@ -74,7 +74,7 @@ describe("USERS SUITE", () => {
     });
   });
 
-  describe("/username SUITE", () => {
+  suite("/username SUITE", () => {
     test("should update username of user with id 1 and return status code 200", async () => {
       const res = await request(URL)
         .put("/api/v1/users/username")
@@ -91,7 +91,7 @@ describe("USERS SUITE", () => {
   });
 });
 
-describe("CHALLENGES SUITE", () => {
+suite("CHALLENGES SUITE", () => {
   test("should get todays challenge and return status code 200", async () => {
     const res = await request(URL).get("/api/v1/challenges/today");
     expect(res.statusCode).toBe(200);
@@ -103,95 +103,140 @@ describe("CHALLENGES SUITE", () => {
   });
 });
 
-describe("COMPLETIONS SUITE", () => {
-  test("should get all completions and return status code 200", async () =>{
+suite("COMPLETIONS SUITE", () => {
+  test("should get all completions and return status code 200", async () => {
     const res = await request(URL).get("/api/v1/completions");
     expect(res.statusCode).toBe(200);
   });
-  
-  test("should get from /unauth_stats and return status code 200", async () =>{
+
+  test("should get from /unauth_stats and return status code 200", async () => {
     const res = await request(URL).get("/api/v1/completions/unauth_stats");
     expect(res.statusCode).toBe(200);
-  })
-
-  test("should get the stats for user with id 1 and return status code 200", async () => {
-    const res = await request(URL).get("/api/v1/completions/stats").send({ id: 1 });
-    expect(res.statusCode).toBe(200);
-  })
-
-  test("should try to get stats from user that doesn't exist and return status code 404", async () => {
-    const res = await request(URL).get("/api/v1/completions/stats").send({ id: 1000000 });
-    expect(res.statusCode).toBe(404);
-  })
-
-  test("should get calendar for user with id of 1 and return status code 200", async () => {
-    const res = await request(URL).get("/api/v1/completions/calendar").send({ id: 1 });
-    expect(res.statusCode).toBe(200);
-  })
-
-  test("should try to get calendar of user that doesn't exist and return status code of 404", async () =>{
-    const res = await request(URL).get("/api/v1/completions/calendar").send({id:100000});
-    expect(res.statusCode).toBe(404);
-  })
-
-  test("should try to get current daily challenge data from user with id of 1 and return status code 200", async () => {
-    const res = await request(URL).get("/api/v1/completions/today").send({ id: 1 });
-    expect(res.statusCode).toBe(200);
   });
 
-  test("should try to get current daily challenge data from user that doesn't exist and return status code 404", async () => {
-    const res = await request(URL).get("/api/v1/completions/today").send({ id: 1000000 });
-    expect(res.statusCode).toBe(404);
+  suite("STATS SUBSUITE", () => {
+    test("should get the stats for user with id 1 and return status code 200", async () => {
+      const res = await request(URL)
+        .get("/api/v1/completions/stats")
+        .send({ id: 1 });
+      expect(res.statusCode).toBe(200);
+    });
+
+    test("should try to get stats from user that doesn't exist and return status code 404", async () => {
+      const res = await request(URL)
+        .get("/api/v1/completions/stats")
+        .send({ id: 1000000 });
+      expect(res.statusCode).toBe(404);
+    });
   });
 
-  test("should get all completions of the day and return status code 200", async () => {
-    const res = await request(URL).get("/api/v1/completions/all_today");
-    expect(res.statusCode).toBe(200);
+  suite("CALENDAR SUBSUITE", () => {
+    test("should get calendar for user with id of 1 and return status code 200", async () => {
+      const res = await request(URL)
+        .get("/api/v1/completions/calendar")
+        .send({ id: 1 });
+      expect(res.statusCode).toBe(200);
+    });
+
+    test("should try to get calendar of user that doesn't exist and return status code of 404", async () => {
+      const res = await request(URL)
+        .get("/api/v1/completions/calendar")
+        .send({ id: 100000 });
+      expect(res.statusCode).toBe(404);
+    });
   });
 
-  test("should get current daily completion from user with id of 1 and return status code 200", async () => {
-    const res = await request(URL).get("/api/v1/completions/1");
-    expect(res.statusCode).toBe(200);
+  suite("TODAY'S CHALLENGE COMPLETION SUBSUITE", () => {
+    test("should try to get current daily challenge data from user with id of 1 and return status code 200", async () => {
+      const res = await request(URL)
+        .get("/api/v1/completions/today")
+        .send({ id: 1 });
+      expect(res.statusCode).toBe(200);
+    });
+
+    test("should try to get current daily challenge data from user that doesn't exist and return status code 404", async () => {
+      const res = await request(URL)
+        .get("/api/v1/completions/today")
+        .send({ id: 1000000 });
+      expect(res.statusCode).toBe(404);
+    });
+
+    test("should get all completions of the day and return status code 200", async () => {
+      const res = await request(URL).get("/api/v1/completions/all_today");
+      expect(res.statusCode).toBe(200);
+    });
+
+    test("should get current daily completion from user with id of 1 and return status code 200", async () => {
+      const res = await request(URL).get("/api/v1/completions/1");
+      expect(res.statusCode).toBe(200);
+    });
+
+    test("should try to get current daily completion from user that doesn't exist and return status code 404", async () => {
+      const res = await request(URL).get("/api/v1/completions/1000000");
+      expect(res.statusCode).toBe(404);
+    });
+
+    test("should send a bad get request and return status code 400", async () => {
+      const res = await request(URL).get("/api/v1/completions/Richard_Nixson");
+      expect(res.statusCode).toBe(400);
+    });
   });
 
-  test("should try to get current daily completion from user that doesn't exist and return status code 404", async () => {
-    const res = await request(URL).get("/api/v1/completions/1000000");
-    expect(res.statusCode).toBe(404);
+  suite("COMPLETION POST SUBSUITE", () => {
+    test("should try to post a new completion for a user who doesn't exist and return status code 404", async () => {
+      const res = await request(URL)
+        .post("/api/v1/completions/")
+        .send({ id: 1000000 });
+      expect(res.statusCode).toBe(404);
+    });
   });
 
-  test("should send a bad get request and return status code 400", async () => {
-    const res = await request(URL).get("/api/v1/completions/Richard_Nixson");
-    expect(res.statusCode).toBe(400);
-  
+  suite("COMPLETION DELETE SUBSUITE", () => {
+    test("should try to delete completion made by used who doesn't exist and return status code 404", async () => {
+      const res = await request(URL)
+        .delete("/api/v1/completions/")
+        .send({ id: 1000000 });
+      expect(res.statusCode).toBe(404);
+    });
+
+    test("should send a bad delete request and return status code 400", async () => {
+      const res = await request(URL).delete(
+        "/api/v1/completions/Richard_Nixson"
+      );
+      expect(res.statusCode).toBe(400);
+    });
   });
 
-  test("should post new completion for user with id of 4", async () => {
-    const res = await request(URL).post("/api/v1/completions/").send({ id: 4, description: "I hate you" });
-    expect(res.statusCode).toBe(200);
-  })
+  suite("COMPLETION ACTUAL POST THEN DELETE SUBSUITE", async () => {
+  // the first two tests as of now can only pass if the database has not been manipulate yet
+  // that is, as of right now, you can only pass these test once unless you want to hardcode the 
+  // completion id in the test below for every test performed
+    test.sequential(
+      "should try to delete post made by user with id of 4 and return status code 204",
+      async () => {
+        const res = await request(URL).delete(`/api/v1/completions/10`);
+        expect(res.statusCode).toBe(204);
+      }
+    );
 
-  test("should post new completion for user who already made a completion and return status code 302", async () => {
-    const res = await request(URL).post("/api/v1/completions/").send({ id: 2});
-    expect(res.statusCode).toBe(302);
+    test.sequential(
+      "should post new completion for user with id of 4",
+      async () => {
+        const res = await request(URL)
+          .post("/api/v1/completions/")
+          .send({ id: 4, description: "I hate you" });
+        expect(res.statusCode).toBe(200);
+      }
+    );
+
+    test.sequential(
+      "should post new completion for user who already made a completion and return status code 302",
+      async () => {
+        const res = await request(URL)
+          .post("/api/v1/completions/")
+          .send({ id: 4 });
+        expect(res.statusCode).toBe(302);
+      }
+    );
   });
-
-  test("should try to post a new completion for a user who doesn't exist and return status code 404", async () => {
-    const res = await request(URL).post("/api/v1/completions/").send({ id: 1000000 });
-    expect(res.statusCode).toBe(404);
-  });
-
-  test("should try to delete post made by user with id of 1 and return status code 204", async () => {
-    const res = await request(URL).delete("/api/v1/completions/").send({ id: 1 });
-    expect(res.statusCode).toBe(204);
-  });
-
-  test("should try to delete completion made by used who doesn't exist and return status code 404", async () => {
-    const res = await request(URL).delete("/api/v1/completions/").send({ id: 1000000 });
-    expect(res.statusCode).toBe(404); 
-  });
-
-  test("should send a bad delete request and return status code 400", async () => {
-    const res = await request(URL).delete("/api/v1/completions/Richard_Nixson");
-    expect(res.statusCode).toBe(400);
-  })
 });
